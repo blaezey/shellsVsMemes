8fb3911af833766da4a1e3391d2e19378093c494
jaydoncastro <89215931+jaydoncastro@users.noreply.github.com>
2022-03-29 08:58:45 -0700

    Updated runner

--- ShellsVsMemes/src/Runner.java
+++ ShellsVsMemes/src/Runner.java
@@ -1,9 +1,122 @@
 
-public class Runner {
 
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+public class Runner implements KeyListener, ActionListener, MouseListener{
+	//I'm just the shell of a man, and a shell of who I was or could've been
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+	}
 
+	public int score;
+	
+	
+	
+	
+	
+	public void paint(Graphics g) {
+		g.drawString("Score: " + score, 500, 500);
+	}
+	
+	
+	public Runner() {
+		JFrame f = new JFrame("Frame");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(800,600);
+		f.add(f, this);
+		f.addKeyListener(this);
+		f.addMouseListener(this);
+		
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setVisible(true);
+		
+	
+		
+	}
+	
+	
+	
+	
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
+	
+	
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
