d156416a556d23cd0f2d8c005db6738d3b663e39
blaezey <60487961+blaezey@users.noreply.github.com>
2022-04-05 11:58:53 -0700

    added frame

--- ShellsVsMemes/src/Runner.java
+++ ShellsVsMemes/src/Runner.java
@@ -1,50 +1,77 @@
-
-
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 
-public class Runner implements KeyListener, ActionListener, MouseListener{
+public class Runner extends JPanel implements KeyListener, ActionListener, MouseListener{
 	//I'm just the shell of a man, and a shell of who I was or could've been
+	
+	Shell[][] board = new Shell[5][9];
+	
+	public int score;
+	File background = new File("ground.png");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+		Runner r = new Runner();
 	}
 
-	public int score;
+	
 	
 	
 	
 	
 	
 	public void paint(Graphics g) {
+		super.repaint();
+		g.setColor(Color.black);
+		g.fillRect(0, 0, 1400, 600);
+		Color textC = new Color(255, 255, 255);
+		g.setColor(textC);
 		g.drawString("Score: " + score, 500, 500);
+		
+		
+		
+		
 	}
 	
 	
 	public Runner() {
-		JFrame f = new JFrame("Frame");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(800,600);
-		f.add(f, this);
-		f.addKeyListener(this);
-		f.addMouseListener(this);
+		JFrame ui = new JFrame();
+		ui.setSize(1400,600);
+		ui.add(this);
 		
+		ui.addKeyListener(this);
+		ui.addMouseListener(this);
+		ui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Timer t = new Timer(16, this);
 		t.start();
-		f.setVisible(true);
-		
-	
+		ui.setVisible(true);
+		ui.repaint();
 		
 	}
 	
