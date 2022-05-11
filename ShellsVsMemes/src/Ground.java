3bae743939173d1dc663284549bdae451e1ff661
blaezey <60487961+blaezey@users.noreply.github.com>
2022-05-11 10:02:28 -0700

    added ui

--- ShellsVsMemes/src/Ground.java
+++ ShellsVsMemes/src/Ground.java
@@ -7,13 +7,16 @@ import java.awt.geom.AffineTransform;
 import java.net.URL;
 
 public class Ground {
-	private int x = 0,y = 0;
-	private Image img; // image of the frog
+	private int x = 0,y = 150, sx = 0, sy = 0;
+	private Image img, shopui; // image of the frog
 	private AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
-
+	private AffineTransform stx = AffineTransform.getTranslateInstance(sx, sy);
+	
 	public Ground() {
-	img = getImage("/imgs/ground.png"); //load the image based on the filename "ground.png"
-		init(x, y);                   //initialize the picture location
+		img = getImage("/imgs/ground.png"); //load the image based on the filename "Background.png"
+		shopui = getImage("/imgs/shopui.png");
+		init(x, y);      //initialize the picture location
+		initS(sx, sy);
 	}
 	
 	
@@ -21,13 +24,19 @@ public class Ground {
 		//using a Graphics2D to draw images
 		Graphics2D g2 = (Graphics2D) g;
 		g2.drawImage(img, tx, null);
-		
+		g2.drawImage(shopui, stx, null);
 	}
 	
 	
 	private void init(double a, double b) {
 		tx.setToTranslation(a, b);
 		tx.scale(1, 1);
+		
+	}
+
+	private void initS(double a, double b) {
+		stx.setToTranslation(a, b);
+		stx.scale(1, 1);
 	}
 
 	// converts image to make it drawable in paint
