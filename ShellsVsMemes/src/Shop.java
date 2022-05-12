c01891eb48f57a575f010d9960a6c0b5028603e7
blaezey <60487961+blaezey@users.noreply.github.com>
2022-05-12 09:55:17 -0700

    updated ui

--- ShellsVsMemes/src/Shop.java
+++ ShellsVsMemes/src/Shop.java
@@ -47,6 +47,17 @@ public class Shop {
 		SnaX = 500;
 		SnaY = -30;
 		
+		ClamX = 440;
+		ClamY = 15;
+		
+		GemX = 250;
+		GemY = 10;
+		
+		OmeX = 150; 
+		OmeY = 43;
+		
+		
+		
 		//add icons for clam, gem, omega
 	}
 	
@@ -54,17 +65,259 @@ public class Shop {
 	
 	
 	
+	public Image getTortoise() {
+		return Tortoise;
+	}
+
+
+	public void setTortoise(Image tortoise) {
+		Tortoise = tortoise;
+	}
+
+
+	public Image getTurtle() {
+		return Turtle;
+	}
+
+
+	public void setTurtle(Image turtle) {
+		Turtle = turtle;
+	}
+
+
+	public Image getArma() {
+		return Arma;
+	}
+
+
+	public void setArma(Image arma) {
+		Arma = arma;
+	}
+
+
+	public Image getSnail() {
+		return Snail;
+	}
+
+
+	public void setSnail(Image snail) {
+		Snail = snail;
+	}
+
+
+	public Image getClam() {
+		return Clam;
+	}
+
+
+	public void setClam(Image clam) {
+		Clam = clam;
+	}
+
+
+	public Image getGemTurtle() {
+		return GemTurtle;
+	}
+
+
+	public void setGemTurtle(Image gemTurtle) {
+		GemTurtle = gemTurtle;
+	}
+
+
+	public Image getOmegaTurtle() {
+		return OmegaTurtle;
+	}
+
+
+	public void setOmegaTurtle(Image omegaTurtle) {
+		OmegaTurtle = omegaTurtle;
+	}
+
+
+	public int getClamX() {
+		return ClamX;
+	}
+
+
+	public void setClamX(int clamX) {
+		ClamX = clamX;
+	}
+
+
+	public int getClamY() {
+		return ClamY;
+	}
+
+
+	public void setClamY(int clamY) {
+		ClamY = clamY;
+	}
+
+
+	public int getGemX() {
+		return GemX;
+	}
+
+
+	public void setGemX(int gemX) {
+		GemX = gemX;
+	}
+
+
+	public int getGemY() {
+		return GemY;
+	}
+
+
+	public void setGemY(int gemY) {
+		GemY = gemY;
+	}
+
+
+	public int getOmeX() {
+		return OmeX;
+	}
+
+
+	public void setOmeX(int omeX) {
+		OmeX = omeX;
+	}
+
+
+	public int getOmeY() {
+		return OmeY;
+	}
+
+
+	public void setOmeY(int omeY) {
+		OmeY = omeY;
+	}
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
+	public AffineTransform getTxTor() {
+		return txTor;
+	}
+
+
+	public void setTxTor(AffineTransform txTor) {
+		this.txTor = txTor;
+	}
+
+
+	public AffineTransform getTxTur() {
+		return txTur;
+	}
+
+
+	public void setTxTur(AffineTransform txTur) {
+		this.txTur = txTur;
+	}
+
+
+	public AffineTransform getTxArm() {
+		return txArm;
+	}
+
+
+	public void setTxArm(AffineTransform txArm) {
+		this.txArm = txArm;
+	}
+
+
+	public AffineTransform getTxSna() {
+		return txSna;
+	}
+
+
+	public void setTxSna(AffineTransform txSna) {
+		this.txSna = txSna;
+	}
+
+
+	public AffineTransform getTxClam() {
+		return txClam;
+	}
+
+
+	public void setTxClam(AffineTransform txClam) {
+		this.txClam = txClam;
+	}
+
+
+	public AffineTransform getTxGem() {
+		return txGem;
+	}
+
+
+	public void setTxGem(AffineTransform txGem) {
+		this.txGem = txGem;
+	}
+
+
+	public AffineTransform getTxOme() {
+		return txOme;
+	}
+
+
+	public void setTxOme(AffineTransform txOme) {
+		this.txOme = txOme;
+	}
+
+
+	public static int getScoreCount() {
+		return scoreCount;
+	}
+
+
+	public static void setScoreCount(int scoreCount) {
+		Shop.scoreCount = scoreCount;
+	}
+
+
 	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;	
 		g2.drawImage(Tortoise, txTor, null);
-		g2.drawImage(Turtle, txTur, null);
+		//g2.drawImage(Turtle, txTur, null);
 		g2.drawImage(Arma, txArm, null);
 		g2.drawImage(Snail, txSna, null);
+		g2.drawImage(Clam, txClam, null);
+		g2.drawImage(GemTurtle, txGem, null);
+		g2.drawImage(OmegaTurtle, txOme, null);
+		
+		
+		
 		
 		txTor.setToTranslation(TorX, TorY);
 		txTur.setToTranslation(TurX, TurY);
 		txArm.setToTranslation(ArmX, ArmY);
 		txSna.setToTranslation(SnaX, SnaY);
+		txClam.setToTranslation(ClamX, ClamY);
+		txGem.setToTranslation(GemX, GemY);
+		txOme.setToTranslation(OmeX, OmeY);
+		
+		
+		
 	}
 	
 	private Image getImage(String path) {
