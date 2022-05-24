46ee03cbdbf33a0a38468de4800a101a5b1ad5d5
blaezey <60487961+blaezey@users.noreply.github.com>
2022-05-24 09:29:32 -0700

    collision

--- ShellsVsMemes/src/Runner.java
+++ ShellsVsMemes/src/Runner.java
@@ -29,8 +29,9 @@ public class Runner extends JPanel implements KeyListener, ActionListener, Mouse
 	ArrayList<Meme> enemies = new ArrayList<Meme>();
 	Meme enemy;
 	
+	int sun = 0;
 	public int score;
-	Ground background = new Ground();
+	Background ground = new Background();
 	Shop snailshop = new Shop();
 	Shell select = null;
 	Music m = new Music("heat.wav", false);
@@ -56,18 +57,21 @@ public class Runner extends JPanel implements KeyListener, ActionListener, Mouse
 		super.repaint();
 		g.setColor(Color.darkGray);
 		g.fillRect(0, 0, 1400, 600);
-		background.paint(g);
+		ground.paint(g);
 		snailshop.paint(g);
 		Color textC = new Color(255, 255, 255);
 		g.setColor(textC);
 		g.drawString("Score: " + score, 1150, 650);
-		
+		g.setColor(Color.black);
+		g.setFont(new Font("Helvetica", 5, 20));
+		g.drawString(sun+"", 59, 135);
 		
 		
 		for(int i = 0; i < board.length; i++) {
 			for(int j = 0; j < board[0].length; j++) {
 				if(board[i][j]!=null) {
 					board[i][j].paint(g);
+					if(board[i][j].getCurrHealth()<=0) board[i][j] = null;
 				}
 			}
 		}
@@ -75,6 +79,20 @@ public class Runner extends JPanel implements KeyListener, ActionListener, Mouse
 		if(enemies.isEmpty()==false) {
 			for(int i = 0; i < enemies.size(); i++) {
 				enemies.get(i).paint(g);
+			}
+			for(int z = enemies.size()-1; z > 0; z--) {
+				for(int i = 0; i < board.length; i++) {
+					for(int j = 0; j < board[0].length; j++) {
+						if(board[i][j]!=null) {
+							if(enemies.get(z).collision(board[i][j])) {
+								board[i][j].setCurrHealth(board[i][j].getCurrHealth()-enemies.get(z).getDamage());
+								enemies.get(z).setTrollX(enemies.get(z).getTrollX()+1);
+							}
+						}
+						
+					}
+				}
+				
 			}
 		}
 		
@@ -127,7 +145,7 @@ public class Runner extends JPanel implements KeyListener, ActionListener, Mouse
 		
 		// TODO Auto-generated method stub
 		if(select==null) {
-			if(arg0.getX()>=146 && arg0.getX()<=228 && arg0.getY()>=61 && arg0.getY()<=132) {
+			if(arg0.getX()>=633 && arg0.getX()<=716 && arg0.getY()>=61 && arg0.getY()<=137) {
 				select = new Shell("Omega Turtle", 0, 0);
 			}else if(arg0.getX()>=246 && arg0.getX()<=328 && arg0.getY()>=57 && arg0.getY()<=133) {
 				select = new Shell("Gem Turtle", 0, 0);
@@ -146,255 +164,255 @@ public class Runner extends JPanel implements KeyListener, ActionListener, Mouse
 			}else if(arg0.getX()>=259 && arg0.getX()<=336 && arg0.getY()>=255 && arg0.getY()<=348) {
 				//set board square to select
 				board[0][0] = select;
-				board[0][0].setOmeX(259);
-				board[0][0].setOmeY(255);
+				board[0][0].setX(259);
+				board[0][0].setY(255);
 				select = null;
 			}else if(arg0.getX()>=339 && arg0.getX()<=408 && arg0.getY()>=244 && arg0.getY()<=348) {
 				//set board square to select
 				board[1][0] = select;
-				board[1][0].setOmeX(339);
-				board[1][0].setOmeY(244);
+				board[1][0].setX(339);
+				board[1][0].setY(244);
 				select = null;
 			}else if(arg0.getX()>=418 && arg0.getX()<=495 && arg0.getY()>=247 && arg0.getY()<=348) {
 				//set board square to select
 				board[2][0] = select;
-				board[2][0].setOmeX(418);
-				board[2][0].setOmeY(247);
+				board[2][0].setX(418);
+				board[2][0].setY(247);
 				
 				select = null;
 			}else if(arg0.getX()>=505 && arg0.getX()<=574 && arg0.getY()>=247 && arg0.getY()<=348){
 				board[3][0] = select;
-				board[3][0].setOmeX(505);
-				board[3][0].setOmeY(247);
+				board[3][0].setX(505);
+				board[3][0].setY(247);
 				select = null;
 			}else if(arg0.getX()>=584 && arg0.getX()<=653 && arg0.getY()>=247 && arg0.getY()<=348){
 				board[4][0] = select;
-				board[4][0].setOmeX(584);
-				board[4][0].setOmeY(247);
+				board[4][0].setX(584);
+				board[4][0].setY(247);
 				select = null;
 			}else if(arg0.getX()>=663 && arg0.getX()<=732 && arg0.getY()>=247 && arg0.getY()<=348){
 				board[5][0] = select;
-				board[5][0].setOmeX(663);
-				board[5][0].setOmeY(247);
+				board[5][0].setX(663);
+				board[5][0].setY(247);
 				select = null;
 			}else if(arg0.getX()>=742 && arg0.getX()<=811 && arg0.getY()>=247 && arg0.getY()<=348){
 				board[6][0] = select;
-				board[6][0].setOmeX(742);
-				board[6][0].setOmeY(247);
+				board[6][0].setX(742);
+				board[6][0].setY(247);
 				select = null;
 			}else if(arg0.getX()>=821 && arg0.getX()<=890 && arg0.getY()>=247 && arg0.getY()<=348){
 				board[7][0] = select;
-				board[7][0].setOmeX(821);
-				board[7][0].setOmeY(247);
+				board[7][0].setX(821);
+				board[7][0].setY(247);
 				select = null;
 			}else if(arg0.getX()>=900 && arg0.getX()<=969 && arg0.getY()>=247 && arg0.getY()<=348){
 				board[8][0] = select;
-				board[8][0].setOmeX(900);
-				board[8][0].setOmeY(247);
+				board[8][0].setX(900);
+				board[8][0].setY(247);
 				select = null;
 			//SECOND ROW BELOW======================================================================
 			}else if(arg0.getX()>=259 && arg0.getX()<=336 && arg0.getY()>=360 && arg0.getY()<=460) {
 				//1
 				board[0][1] = select;
-				board[0][1].setOmeX(259);
-				board[0][1].setOmeY(350);
+				board[0][1].setX(259);
+				board[0][1].setY(350);
 				select = null;
 			}else if(arg0.getX()>=339 && arg0.getX()<=408 && arg0.getY()>=360 && arg0.getY()<=460) {
 				//set board square to select
 				board[1][1] = select;
-				board[1][1].setOmeX(339);
-				board[1][1].setOmeY(350);
+				board[1][1].setX(339);
+				board[1][1].setY(350);
 				select = null;
 			}else if(arg0.getX()>=418 && arg0.getX()<=495 && arg0.getY()>=360 && arg0.getY()<=460) {
 				//3
 				board[2][1] = select;
-				board[2][1].setOmeX(418);
-				board[2][1].setOmeY(350);
+				board[2][1].setX(418);
+				board[2][1].setY(350);
 				
 				select = null;
 			}else if(arg0.getX()>=505 && arg0.getX()<=574 && arg0.getY()>=360 && arg0.getY()<=460){
 				//set board square to select
 				board[3][1] = select;
-				board[3][1].setOmeX(500);
-				board[3][1].setOmeY(350);
+				board[3][1].setX(500);
+				board[3][1].setY(350);
 				select = null;
 			}else if(arg0.getX()>=584 && arg0.getX()<=653 && arg0.getY()>=360 && arg0.getY()<=460){
 				board[4][1] = select;
-				board[4][1].setOmeX(570);
-				board[4][1].setOmeY(350);
+				board[4][1].setX(570);
+				board[4][1].setY(350);
 				select = null;
 			}else if(arg0.getX()>=663 && arg0.getX()<=732 && arg0.getY()>=360 && arg0.getY()<=460){
 				board[5][1] = select;
-				board[5][1].setOmeX(663);
-				board[5][1].setOmeY(350);
+				board[5][1].setX(663);
+				board[5][1].setY(350);
 				select = null;
 			}else if(arg0.getX()>=742 && arg0.getX()<=811 && arg0.getY()>=360 && arg0.getY()<=460){
 				board[6][1] = select;
-				board[6][1].setOmeX(742);
-				board[6][1].setOmeY(350);
+				board[6][1].setX(742);
+				board[6][1].setY(350);
 				select = null;
 			}else if(arg0.getX()>=821 && arg0.getX()<=890 && arg0.getY()>=360 && arg0.getY()<=460){
 				board[7][1] = select;
-				board[7][1].setOmeX(821);
-				board[7][1].setOmeY(350);
+				board[7][1].setX(821);
+				board[7][1].setY(350);
 				select = null;
 			}else if(arg0.getX()>=900 && arg0.getX()<=969 && arg0.getY()>=360 && arg0.getY()<=460){
 				board[8][1] = select;
-				board[8][1].setOmeX(900);
-				board[8][1].setOmeY(350);
+				board[8][1].setX(900);
+				board[8][1].setY(350);
 				select = null;
 				//START OF THIRD ROW===============================================================================
 			}else if(arg0.getX()>=259 && arg0.getX()<=336 && arg0.getY()>=462 && arg0.getY()<=550) {
 				//1
 				board[0][2] = select;
-				board[0][2].setOmeX(259);
-				board[0][2].setOmeY(462);
+				board[0][2].setX(259);
+				board[0][2].setY(462);
 				select = null;
 			}else if(arg0.getX()>=339 && arg0.getX()<=408 && arg0.getY()>=462 && arg0.getY()<=550) {
 				//set board square to select
 				board[1][2] = select;
-				board[1][2].setOmeX(339);
-				board[1][2].setOmeY(462);
+				board[1][2].setX(339);
+				board[1][2].setY(462);
 				select = null;
 			}else if(arg0.getX()>=418 && arg0.getX()<=495 && arg0.getY()>=462 && arg0.getY()<=550) {
 				//3
 				board[2][2] = select;
-				board[2][2].setOmeX(418);
-				board[2][2].setOmeY(462);
+				board[2][2].setX(418);
+				board[2][2].setY(462);
 				
 				select = null;
 			}else if(arg0.getX()>=505 && arg0.getX()<=574 && arg0.getY()>=462 && arg0.getY()<=550){
 				//set board square to select
 				board[3][2] = select;
-				board[3][2].setOmeX(500);
-				board[3][2].setOmeY(462);
+				board[3][2].setX(500);
+				board[3][2].setY(462);
 				select = null;
 			}else if(arg0.getX()>=584 && arg0.getX()<=653 && arg0.getY()>=462 && arg0.getY()<=550){
 				board[4][2] = select;
-				board[4][2].setOmeX(580);
-				board[4][2].setOmeY(462);
+				board[4][2].setX(580);
+				board[4][2].setY(462);
 				select = null;
 			}else if(arg0.getX()>=663 && arg0.getX()<=732 && arg0.getY()>=462 && arg0.getY()<=550){
 				board[5][2] = select;
-				board[5][2].setOmeX(663);
-				board[5][2].setOmeY(462);
+				board[5][2].setX(663);
+				board[5][2].setY(462);
 				select = null;
 			}else if(arg0.getX()>=742 && arg0.getX()<=811 && arg0.getY()>=462 && arg0.getY()<=550){
 				board[6][2] = select;
-				board[6][2].setOmeX(742);
-				board[6][2].setOmeY(462);
+				board[6][2].setX(742);
+				board[6][2].setY(462);
 				select = null;
 			}else if(arg0.getX()>=821 && arg0.getX()<=890 && arg0.getY()>=462 && arg0.getY()<=550){
 				board[7][2] = select;
-				board[7][2].setOmeX(821);
-				board[7][2].setOmeY(462);
+				board[7][2].setX(821);
+				board[7][2].setY(462);
 				select = null;
 			}else if(arg0.getX()>=900 && arg0.getX()<=969 && arg0.getY()>=462 && arg0.getY()<=550){
 				board[8][2] = select;
-				board[8][2].setOmeX(900);
-				board[8][2].setOmeY(462);
+				board[8][2].setX(900);
+				board[8][2].setY(462);
 				select = null;
 				//START OF 4TH ROW===============================================================================
 			}else if(arg0.getX()>=259 && arg0.getX()<=336 && arg0.getY()>=560 && arg0.getY()<=650) {
 				//1
 				board[0][3] = select;
-				board[0][3].setOmeX(259);
-				board[0][3].setOmeY(560);
+				board[0][3].setX(259);
+				board[0][3].setY(560);
 				select = null;
 			}else if(arg0.getX()>=339 && arg0.getX()<=408 && arg0.getY()>=560 && arg0.getY()<=650) {
 				//set board square to select
 				board[1][3] = select;
-				board[1][3].setOmeX(339);
-				board[1][3].setOmeY(560);
+				board[1][3].setX(339);
+				board[1][3].setY(560);
 				select = null;
 			}else if(arg0.getX()>=418 && arg0.getX()<=495 && arg0.getY()>=560 && arg0.getY()<=650) {
 				//3
 				board[2][3] = select;
-				board[2][3].setOmeX(418);
-				board[2][3].setOmeY(560);
+				board[2][3].setX(418);
+				board[2][3].setY(560);
 				
 				select = null;
 			}else if(arg0.getX()>=505 && arg0.getX()<=574 && arg0.getY()>=560 && arg0.getY()<=650){
 				//set board square to select
 				board[3][3] = select;
-				board[3][3].setOmeX(500);
-				board[3][3].setOmeY(560);
+				board[3][3].setX(500);
+				board[3][3].setY(560);
 				select = null;
 			}else if(arg0.getX()>=584 && arg0.getX()<=653 && arg0.getY()>=560 && arg0.getY()<=650){
 				board[4][3] = select;
-				board[4][3].setOmeX(580);
-				board[4][3].setOmeY(560);
+				board[4][3].setX(580);
+				board[4][3].setY(560);
 				select = null;
 			}else if(arg0.getX()>=663 && arg0.getX()<=732 && arg0.getY()>=560 && arg0.getY()<=650){
 				board[5][3] = select;
-				board[5][3].setOmeX(663);
-				board[5][3].setOmeY(560);
+				board[5][3].setX(663);
+				board[5][3].setY(560);
 				select = null;
 			}else if(arg0.getX()>=742 && arg0.getX()<=811 && arg0.getY()>=560 && arg0.getY()<=650){
 				board[6][3] = select;
-				board[6][3].setOmeX(742);
-				board[6][3].setOmeY(560);
+				board[6][3].setX(742);
+				board[6][3].setY(560);
 				select = null;
 			}else if(arg0.getX()>=821 && arg0.getX()<=890 && arg0.getY()>=560 && arg0.getY()<=650){
 				board[7][3] = select;
-				board[7][3].setOmeX(821);
-				board[7][3].setOmeY(560);
+				board[7][3].setX(821);
+				board[7][3].setY(560);
 				select = null;
 			}else if(arg0.getX()>=900 && arg0.getX()<=969 && arg0.getY()>=560 && arg0.getY()<=650){
 				board[8][3] = select;
-				board[8][3].setOmeX(900);
-				board[8][3].setOmeY(560);
+				board[8][3].setX(900);
+				board[8][3].setY(560);
 				select = null;
 				//LAST ROW YEAHHHH BABYYYY===============================================================================
 			}else if(arg0.getX()>=259 && arg0.getX()<=336 && arg0.getY()>=655 && arg0.getY()<=750) {
 				//1
 				board[0][4] = select;
-				board[0][4].setOmeX(259);
-				board[0][4].setOmeY(655);
+				board[0][4].setX(259);
+				board[0][4].setY(655);
 				select = null;
 			}else if(arg0.getX()>=339 && arg0.getX()<=408 && arg0.getY()>=655 && arg0.getY()<=750) {
 				//set board square to select
 				board[1][4] = select;
-				board[1][4].setOmeX(339);
-				board[1][4].setOmeY(655);
+				board[1][4].setX(339);
+				board[1][4].setY(655);
 				select = null;
 			}else if(arg0.getX()>=418 && arg0.getX()<=495 && arg0.getY()>=655 && arg0.getY()<=750) {
 				//3
 				board[2][4] = select;
-				board[2][4].setOmeX(418);
-				board[2][4].setOmeY(655);
+				board[2][4].setX(418);
+				board[2][4].setY(655);
 				
 				select = null;
 			}else if(arg0.getX()>=505 && arg0.getX()<=574 && arg0.getY()>=655 && arg0.getY()<=750){
 				//set board square to select
 				board[3][4] = select;
-				board[3][4].setOmeX(500);
-				board[3][4].setOmeY(655);
+				board[3][4].setX(500);
+				board[3][4].setY(655);
 				select = null;
 			}else if(arg0.getX()>=584 && arg0.getX()<=653 && arg0.getY()>=655 && arg0.getY()<=750){
 				board[4][4] = select;
-				board[4][4].setOmeX(580);
-				board[4][4].setOmeY(655);
+				board[4][4].setX(580);
+				board[4][4].setY(655);
 				select = null;
 			}else if(arg0.getX()>=663 && arg0.getX()<=732 && arg0.getY()>=655 && arg0.getY()<=750){
 				board[5][4] = select;
-				board[5][4].setOmeX(663);
-				board[5][4].setOmeY(655);
+				board[5][4].setX(663);
+				board[5][4].setY(655);
 				select = null;
 			}else if(arg0.getX()>=742 && arg0.getX()<=811 && arg0.getY()>=655 && arg0.getY()<=750){
 				board[6][4] = select;
-				board[6][4].setOmeX(742);
-				board[6][4].setOmeY(655);
+				board[6][4].setX(742);
+				board[6][4].setY(655);
 				select = null;
 			}else if(arg0.getX()>=821 && arg0.getX()<=890 && arg0.getY()>=655 && arg0.getY()<=750){
 				board[7][4] = select;
-				board[7][4].setOmeX(821);
-				board[7][4].setOmeY(655);
+				board[7][4].setX(821);
+				board[7][4].setY(655);
 				select = null;
 			}else if(arg0.getX()>=900 && arg0.getX()<=969 && arg0.getY()>=655 && arg0.getY()<=750){
 				board[8][4] = select;
-				board[8][4].setOmeX(900);
-				board[8][4].setOmeY(655);
+				board[8][4].setX(900);
+				board[8][4].setY(655);
 				select = null;
 			}else {
 				select = null;
